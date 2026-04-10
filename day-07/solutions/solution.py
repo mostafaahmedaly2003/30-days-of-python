51d00e25591b3ba4aa974e12b6667abaa8f310ef
Mostafa Ahmed <mostafaahmedaly2003@gmail.com>
2026-04-11 01:30:30 +0200

    Add day-07: solution

---
+++ day-07/solutions/solution.py
@@ -0,0 +1,12 @@
+# Day 07 - for Loops & range() | SOLUTION
+# Try to solve exercises.py yourself before looking here!
+
+for i in range(1, 31):
+    if i % 15 == 0:
+        print('FizzBuzz')
+    elif i % 3 == 0:
+        print('Fizz')
+    elif i % 5 == 0:
+        print('Buzz')
+    else:
+        print(i)
