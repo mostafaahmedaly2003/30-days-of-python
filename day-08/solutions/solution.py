264f1ed41cacee9e8fd857acab2f657500396d9a
Mostafa Ahmed <mostafaahmedaly2003@gmail.com>
2026-04-11 01:30:34 +0200

    Add day-08: solution

---
+++ day-08/solutions/solution.py
@@ -0,0 +1,10 @@
+# Day 08 - Lists | SOLUTION
+# Try to solve exercises.py yourself before looking here!
+
+fruits = ['apple', 'banana', 'cherry']
+fruits.append('mango')
+fruits.insert(1, 'blueberry')
+fruits.remove('banana')
+print(fruits)
+print(fruits[0], fruits[-1])
+print(sorted(fruits))
