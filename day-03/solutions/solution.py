7826bd48f759e3d6cf65d924d69d1c8229d64f6b
Mostafa Ahmed <mostafaahmedaly2003@gmail.com>
2026-04-11 01:30:12 +0200

    Add day-03: solution

---
+++ day-03/solutions/solution.py
@@ -0,0 +1,7 @@
+# Day 03 - User Input, Type Conversion & f-strings | SOLUTION
+# Try to solve exercises.py yourself before looking here!
+
+name = input('Enter your name: ')
+age = int(input('Enter your age: '))
+birth_year = 2026 - age
+print(f'Hello {name}! You were born in {birth_year}.')
