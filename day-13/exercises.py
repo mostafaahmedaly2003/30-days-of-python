16ca0976e58c5226efd05492a81d93b113fb39ce
Mostafa Ahmed <mostafaahmedaly2003@gmail.com>
2026-04-11 01:30:55 +0200

    Add day-13: exercises.py

---
+++ day-13/exercises.py
@@ -0,0 +1,23 @@
+# Day 13 - Functions Advanced - args kwargs lambda
+# Complete each exercise below. Write your code under each comment.
+
+# Exercise 1: function that accepts any number of positional arguments
+# YOUR CODE HERE:
+
+
+
+# Exercise 2: function using **kwargs to print a profile
+# YOUR CODE HERE:
+
+
+
+# Exercise 3: lambda function for sorting a list of dicts
+# YOUR CODE HERE:
+
+
+
+# Exercise 4: use map() and filter() with lambda
+# YOUR CODE HERE:
+
+
+
