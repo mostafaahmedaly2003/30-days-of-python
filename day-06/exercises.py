b0e1a80de192f131c65d87f21fd0e919d773a3c4
Mostafa Ahmed <mostafaahmedaly2003@gmail.com>
2026-04-11 01:30:24 +0200

    Add day-06: exercises.py

---
+++ day-06/exercises.py
@@ -0,0 +1,23 @@
+# Day 06 - while Loops
+# Complete each exercise below. Write your code under each comment.
+
+# Exercise 1: count from 1 to 10 using while
+# YOUR CODE HERE:
+
+
+
+# Exercise 2: sum numbers until user types 0
+# YOUR CODE HERE:
+
+
+
+# Exercise 3: number guessing game (1-100)
+# YOUR CODE HERE:
+
+
+
+# Exercise 4: print multiplication table for any number
+# YOUR CODE HERE:
+
+
+
