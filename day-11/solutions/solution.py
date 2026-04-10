59eedb07a476910794695ab1255ac1f9cbf8cf61
Mostafa Ahmed <mostafaahmedaly2003@gmail.com>
2026-04-11 01:30:47 +0200

    Add day-11: solution

---
+++ day-11/solutions/solution.py
@@ -0,0 +1,10 @@
+# Day 11 - String Methods | SOLUTION
+# Try to solve exercises.py yourself before looking here!
+
+text = '  Hello, World!  '
+print(text.strip())
+print(text.lower())
+print(text.upper())
+print(text.replace('World', 'Python'))
+print(','.join(['a','b','c']))
+print('hello world'.title())
