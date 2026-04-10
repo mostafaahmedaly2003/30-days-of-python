94c694d6295ec765fe904e34df8e1b0124070eb4
Mostafa Ahmed <mostafaahmedaly2003@gmail.com>
2026-04-11 01:30:21 +0200

    Add day-05: solution

---
+++ day-05/solutions/solution.py
@@ -0,0 +1,14 @@
+# Day 05 - Conditionals - if elif else | SOLUTION
+# Try to solve exercises.py yourself before looking here!
+
+grade = int(input('Score: '))
+if grade >= 90:
+    print('A')
+elif grade >= 80:
+    print('B')
+elif grade >= 70:
+    print('C')
+elif grade >= 60:
+    print('D')
+else:
+    print('F')
