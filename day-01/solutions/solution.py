a3b01a82a762b0990739202a4889a73400c10488
Mostafa Ahmed <mostafaahmedaly2003@gmail.com>
2026-04-11 01:30:03 +0200

    Add day-01: solution

---
+++ day-01/solutions/solution.py
@@ -0,0 +1,8 @@
+# Day 01 - Setup, Hello World & print() | SOLUTION
+# Try to solve exercises.py yourself before looking here!
+
+print('Hello, World!')
+# Single-line comment
+print('Name: Mostafa')
+print('Age: 22')
+print('City: Cairo')
