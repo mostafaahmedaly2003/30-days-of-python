b6b337588260ead2a6163e5b1f2c25d4ce7f4a0c
Mostafa Ahmed <mostafaahmedaly2003@gmail.com>
2026-04-11 01:30:07 +0200

    Add day-02: solution

---
+++ day-02/solutions/solution.py
@@ -0,0 +1,11 @@
+# Day 02 - Variables & Data Types | SOLUTION
+# Try to solve exercises.py yourself before looking here!
+
+name = 'Mostafa'
+age = 22
+height = 1.75
+is_student = True
+print(type(name), type(age), type(height), type(is_student))
+a, b = 5, 10
+a, b = b, a
+print(a, b)
