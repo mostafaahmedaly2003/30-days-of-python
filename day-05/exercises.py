e98e05c58725e8bab5c590564bb0acd47e123f71
Mostafa Ahmed <mostafaahmedaly2003@gmail.com>
2026-04-11 01:30:19 +0200

    Add day-05: exercises.py

---
+++ day-05/exercises.py
@@ -0,0 +1,23 @@
+# Day 05 - Conditionals - if elif else
+# Complete each exercise below. Write your code under each comment.
+
+# Exercise 1: grade classifier: A/B/C/D/F from score
+# YOUR CODE HERE:
+
+
+
+# Exercise 2: check if a year is a leap year
+# YOUR CODE HERE:
+
+
+
+# Exercise 3: BMI calculator with category (underweight/normal/overweight/obese)
+# YOUR CODE HERE:
+
+
+
+# Exercise 4: simple login check (username + password)
+# YOUR CODE HERE:
+
+
+
