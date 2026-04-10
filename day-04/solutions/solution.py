e945dc8302a84865ac9421385996a0e1062cb7b9
Mostafa Ahmed <mostafaahmedaly2003@gmail.com>
2026-04-11 01:30:16 +0200

    Add day-04: solution

---
+++ day-04/solutions/solution.py
@@ -0,0 +1,7 @@
+# Day 04 - Operators | SOLUTION
+# Try to solve exercises.py yourself before looking here!
+
+print(10 + 3, 10 - 3, 10 * 3, 10 / 3, 10 // 3, 10 % 3, 10 ** 3)
+n = 7
+print('even' if n % 2 == 0 else 'odd')
+print(1 < 2 < 3)
