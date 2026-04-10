d601f508b6bdf83dd4611d582fb812802a97a3ec
Mostafa Ahmed <mostafaahmedaly2003@gmail.com>
2026-04-11 01:30:28 +0200

    Add day-07: exercises.py

---
+++ day-07/exercises.py
@@ -0,0 +1,23 @@
+# Day 07 - for Loops & range()
+# Complete each exercise below. Write your code under each comment.
+
+# Exercise 1: print squares of numbers 1-10
+# YOUR CODE HERE:
+
+
+
+# Exercise 2: FizzBuzz from 1 to 30
+# YOUR CODE HERE:
+
+
+
+# Exercise 3: sum all even numbers from 1 to 100
+# YOUR CODE HERE:
+
+
+
+# Exercise 4: reverse a string using a for loop
+# YOUR CODE HERE:
+
+
+
