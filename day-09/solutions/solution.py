6431582e812a6f0614d63e2a1817bbbf5b5850ae
Mostafa Ahmed <mostafaahmedaly2003@gmail.com>
2026-04-11 01:30:39 +0200

    Add day-09: solution

---
+++ day-09/solutions/solution.py
@@ -0,0 +1,11 @@
+# Day 09 - Tuples & Sets | SOLUTION
+# Try to solve exercises.py yourself before looking here!
+
+coords = (40.7128, -74.0060)
+lat, lon = coords
+print(f'Lat: {lat}, Lon: {lon}')
+a = {1,2,3,4}
+b = {3,4,5,6}
+print(a & b)
+print(a | b)
+print(a - b)
