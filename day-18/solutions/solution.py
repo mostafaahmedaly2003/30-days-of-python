64017daf4cb44fd946bdbf3f11f5c23f0b75e326
Mostafa Ahmed <mostafaahmedaly2003@gmail.com>
2026-04-11 01:31:20 +0200

    Add day-18: solution

---
+++ day-18/solutions/solution.py
@@ -0,0 +1,11 @@
+# Day 18 - Modules & Packages | SOLUTION
+# Try to solve exercises.py yourself before looking here!
+
+import random
+import math
+import os
+print(random.randint(1,100))
+print(math.pi, math.sqrt(16))
+print(os.getcwd())
+if __name__ == '__main__':
+    print('Running as main script')
