4aca04d7b3b5ddf79cb97b2996998ee2dcec9605
Mostafa Ahmed <mostafaahmedaly2003@gmail.com>
2026-04-11 01:32:03 +0200

    Add day-28: exercises.py

---
+++ day-28/exercises.py
@@ -0,0 +1,23 @@
+# Day 28 - Data Visualization with matplotlib
+# Complete each exercise below. Write your code under each comment.
+
+# Exercise 1: bar chart of top 5 programming languages by popularity
+# YOUR CODE HERE:
+
+
+
+# Exercise 2: line chart of temperature over 7 days
+# YOUR CODE HERE:
+
+
+
+# Exercise 3: pie chart of time spent studying different subjects
+# YOUR CODE HERE:
+
+
+
+# Exercise 4: scatter plot of two numerical variables
+# YOUR CODE HERE:
+
+
+
