df5974370723d55e0a088c888324a0cf2753808e
Mostafa Ahmed <mostafaahmedaly2003@gmail.com>
2026-04-11 01:31:09 +0200

    Add day-16: exercises.py

---
+++ day-16/exercises.py
@@ -0,0 +1,23 @@
+# Day 16 - File IO
+# Complete each exercise below. Write your code under each comment.
+
+# Exercise 1: write a list of names to a file, one per line
+# YOUR CODE HERE:
+
+
+
+# Exercise 2: read a file and count total lines and words
+# YOUR CODE HERE:
+
+
+
+# Exercise 3: append new entries to an existing file
+# YOUR CODE HERE:
+
+
+
+# Exercise 4: copy contents of one file to another
+# YOUR CODE HERE:
+
+
+
